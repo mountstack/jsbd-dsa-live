64113756434c91d579a47cc293ef1f07b3b2dcc9
MOHAMMAD ERFAN HOSSAIN <shoaibeee05@gmail.com>
2023-01-31 00:54:39 +0600

    graph traversal

--- Graph/graph.js
+++ Graph/graph.js
@@ -2,9 +2,14 @@
 class Graph {
     constructor() {
         this.graph = {}; 
+        this.visited = {}; 
     } 
 
-    add(a, b) { 
+    add(a, b, dir) { 
+        this.visited[a] = false; 
+        this.visited[b] = false; 
+
+
         // 1st part
         if(this.graph[a]) {
             this.graph[a].push(b); 
@@ -20,25 +25,59 @@ class Graph {
         else {
             this.graph[b] = [a]; 
         }
-    }
+    } 
+
+
+    bfs(src) {
+        let queue = []; 
+        queue.push(src); 
+        this.visited[src] = true;  
+
+        while(queue.length) { 
+            let data = queue[0]; 
+            console.log(data); 
+
+            let arr = this.graph[data]; 
+            for(let i = 0; i < arr.length; i++) {
+                if(!this.visited[arr[i]]) {
+                    queue.push(arr[i]); 
+                    this.visited[arr[i]] = true; 
+                }
+            }
+
+            queue.shift(); 
+        }
+    } 
 }
 
 
 let gp = new Graph(); 
 
-gp.add(5, 4); 
-gp.add(5, 1); 
-gp.add(1, 2); 
-gp.add(2, 3); 
-gp.add(3, 4); 
-gp.add(1, 6); 
-gp.add(6, 5); 
-gp.add(6, 7); 
-gp.add(7, 2); 
-gp.add(1, 4); 
+gp.add(1, 2)
+gp.add(1, 3)
+gp.add(2, 3)
+gp.add(4, 3)
+gp.add(2, 4)
+gp.add(2, 5)
+gp.add(4, 5)
+gp.add(5, 6)
 
-console.log(gp.graph);
+gp.add(7, 8)
+gp.add(7, 9)
+gp.add(8, 9)
 
 
+
+// console.log(gp.graph);
+
+let count = 0; 
+for(let abc in gp.visited) { 
+    if(!gp.visited[abc]) {
+        count++; 
+        gp.bfs(abc)
+    }
+}
+
+console.log({count}); 
 
 
